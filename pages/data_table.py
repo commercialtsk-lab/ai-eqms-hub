f210af8e60477b36714d08c85cb34e4f729f4f69
commercialtsk-lab <commercialtsk@gmail.com>
2026-08-16 14:45:18 +0530

    Fix import errors in data_table and exports

--- pages/data_table.py
+++ pages/data_table.py
@@ -1,1 +1,1 @@
-from utils.exports import generate_pdf, create_table_image, build_whatsapp_message, get_pnr_status_url
+from utils.exports import generate_pdf, create_table_image, build_whatsapp_message, get_pnr_status_url 
